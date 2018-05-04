00e025920b7dddf54f4245c560b6d9d57932b657
fatimahseraj <fatimahseraj2@hotmail.com>
2018-05-05 02:44:05 +0300

    remove service

--- FatmaProject/src/HelloWorldService.java
+++ FatmaProject/src/HelloWorldService.java
@@ -1,14 +1,4 @@
 
-import javax.servlet.http.HttpServlet;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-@Path("/hello")
-public class HelloWorldService extends HttpServlet{
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String sayHello() {
-        return "Hello Jersey";
-    }
+public class HelloWorldService {
+
 }
